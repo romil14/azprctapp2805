59c051c7a133d110cc39ed87340aa9b1c0fa5594
romil-sd <romil.chandragade@aquariansoft.in>
2021-05-28 07:20:24 +0530

    Project Created

---
+++ az_webAppDb_ass280521/az_webAppDb_ass280521/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace az_webAppDb_ass280521.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
